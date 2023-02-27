e267a932af9973cccf74772a0afaebab012786f5
ouharri <otmanouhrri@gmail.com>
2023-02-27 23:04:03 +0000

    update v9.5

--- resources/views/components/nav/left_sidebar.blade.php
+++ resources/views/components/nav/left_sidebar.blade.php
@@ -90,7 +90,7 @@
         </a>
         <a class=" flex items-center space-x-2 py-1 mt-4" href="#">
             <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-                <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 border-gray-300 appearance-none cursor-pointer"  @click="isDark = !isDark" :value="isDark"/>
+                <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 border-gray-300 appearance-none cursor-pointer"  @click="isDark = !isDark" />
                 <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
             </div>
             <label for="toggle" class="">Dark Theme</label>
